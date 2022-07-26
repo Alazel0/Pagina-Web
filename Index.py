0bea8eb4a0529d4457d75a6e4b65d1c13e7afa8f
Alazel0 <alexandervenegas25@gmail.com>
2022-07-26 18:28:57 -0300

    lll

--- Index.py
+++ Index.py
@@ -13,4 +13,4 @@ def NeoCab():
     return render_template('Neo Cab.html')
 
 if __name__=='__main__':
-    app.run(debug=True,host='0.0.0.0',port='5000')
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 5000)))
